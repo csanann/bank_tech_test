73a8e45be54c2be49664a6063b3e5666b7b7c4bb
csanann <csanann@gmail.com>
2023-06-22 22:42:25 +0100

    Adds app file-hardcode

---
+++ app.js
@@ -0,0 +1,14 @@
+//file: app.js
+
+// file: app.js
+
+const BankTransaction = require('./src/bank_transaction');
+const BankStatement = require('./src/bank_statement');
+
+const bankTransaction = new BankTransaction();
+bankTransaction.deposit(1000);
+bankTransaction.deposit(2000);
+bankTransaction.withdraw(500);
+
+const bankStatement = new BankStatement(bankTransaction.transactions);
+bankStatement.printStatement();
